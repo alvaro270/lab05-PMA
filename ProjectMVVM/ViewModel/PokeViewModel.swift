6996286ea89efd7dd410e691e3419c833ebe77c4
alvaro270 <alvarogamer270@gmail.com>
2022-04-28 21:44:06 -0500

    lab05

---
+++ ProjectMVVM/ViewModel/PokeViewModel.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+class PokeViewModel {
+    
+    let URL_API = "https://pokeapi.co/api/v2/pokemon?limit=100"
+    
+    var pokemons = [Result]()
+    
+    func getDataFromAPI() async {
+        
+        guard let url = URL(string: URL_API) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decoder = try? JSONDecoder().decode(Pokemon.self, from: data) {
+               
+                DispatchQueue.main.async(execute: {
+                    decoder.results.forEach { pokemon in
+                        self.pokemons.append(pokemon)
+                    }
+                })
+            }
+        } catch {
+            print("Ivalid error")
+        }
+        
+    }
+    
+}
